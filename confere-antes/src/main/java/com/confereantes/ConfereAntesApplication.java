0025082898d52804d8a0488656b2c535cd2cf35a
tavin27 <otavioaugsilv@gmail.com>
2026-09-12 03:50:09 -0300

    chore: configuração projeto Spring Boot e MySQL

---
+++ confere-antes/src/main/java/com/confereantes/ConfereAntesApplication.java
@@ -0,0 +1,13 @@
+package com.confereantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfereAntesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfereAntesApplication.class, args);
+	}
+
+}
