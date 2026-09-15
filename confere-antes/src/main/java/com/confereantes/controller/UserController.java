4308fff38e0f8dfab690dfc6a6d75287888f8cc3
tavin27 <otavioaugsilv@gmail.com>
2026-09-15 04:06:23 -0300

    feat: add user creation endpoint

---
+++ confere-antes/src/main/java/com/confereantes/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.confereantes.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.confereantes.service.UserService;
+import com.confereantes.model.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    public User save(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+}
