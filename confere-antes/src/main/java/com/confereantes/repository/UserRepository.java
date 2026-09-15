58c7121abbe7d515e2d8e0cd9f5368e38ae585f0
tavin27 <otavioaugsilv@gmail.com>
2026-09-15 02:48:50 -0300

    feat: add UserRepository

---
+++ confere-antes/src/main/java/com/confereantes/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.confereantes.repository;
+
+import com.confereantes.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
