98a01ca032094af63c3b99f270d0650697045ceb
tavin27 <otavioaugsilv@gmail.com>
2026-09-15 02:21:17 -0300

    feat: cria entidade User

---
+++ confere-antes/src/main/java/com/confereantes/model/User.java
@@ -0,0 +1,28 @@
+package com.confereantes.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String phone;
+    private String cpf;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+}
